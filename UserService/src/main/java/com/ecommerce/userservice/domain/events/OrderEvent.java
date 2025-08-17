f58417bbaf9f89879da8e6eba793080bb8b03314
ahmetrecayiozturk <ahmetrecayiozturk@gmail.com>
2025-08-17 04:28:31 +0300

    User Service Updated

---
+++ UserService/src/main/java/com/ecommerce/userservice/domain/events/OrderEvent.java
@@ -0,0 +1,21 @@
+package com.ecommerce.userservice.domain.events;
+
+import com.ecommerce.userservice.domain.model.ProductSnapshot;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Data;
+
+@Data
+public class OrderEvent {
+    @JsonAlias({"id"})
+    private Long orderId;
+    private Long userId;
+    private Long productId;
+    private int quantity;
+    private ProductSnapshot productSnapshot; // yeni alan
+
+    public Long getProductId() {
+        if (productId != null) return productId;
+        if (productSnapshot != null && productSnapshot.getProductId() != null) return productSnapshot.getProductId();
+        return null;
+    }
+}
---
+++ UserService/src/main/java/com/ecommerce/userservice/domain/model/ProductSnapshot.java
@@ -0,0 +1,19 @@
+package com.ecommerce.userservice.domain.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class ProductSnapshot {
+    private Long productId;
+    private String name;
+    private Double price;
+
+    public ProductSnapshot(Long productId, String name, Double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+    public ProductSnapshot() {}
+}
