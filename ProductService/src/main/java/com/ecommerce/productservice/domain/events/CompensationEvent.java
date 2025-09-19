a6ceb7ea1e5885a9eca51e69e919fb8071a949b0
Ahmet Recayi <149142851+ahmetrecayiozturk@users.noreply.github.com>
2025-09-19 12:55:41 +0300

    Update CompensationEvent.java

--- ProductService/src/main/java/com/ecommerce/productservice/domain/events/CompensationEvent.java
+++ ProductService/src/main/java/com/ecommerce/productservice/domain/events/CompensationEvent.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class CompensationEvent {
-    private Long userId; // Optional, if specific user compensation is needed
+    private Long userId; 
     private Long orderId;
-    private Long productId; // Optional, if specific product compensation is needed
-    private int quantity; // Optional, if specific quantity compensation is needed
-    private String failedStep; // "PAYMENT", "PRODUCT", "DELIVERY" vs.
+    private Long productId; 
+    private int quantity; 
+    private String failedStep;
     private String reason;}
